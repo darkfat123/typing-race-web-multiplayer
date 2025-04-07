f16d49cb823df1d9622195851795a9c471826401
darkfat123 <supakorn.yookack@gmail.com>
2025-04-07 18:05:45 +0700

    Remove dotenv loading from main function

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -5,15 +5,9 @@ import (
 	"log"
 	"net/http"
 	"server/internal/handler"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 	http.HandleFunc("/ws/lobby", handler.HandleLobbyWebSocket)
 	http.HandleFunc("/ws/typing", handler.HandleTypingWebSocket)
 	fmt.Println("Server started on :8080")
